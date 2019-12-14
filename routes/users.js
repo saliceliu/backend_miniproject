378f50aca10f61d92b20a06171eeff9d84d8ced1
Rosie Liu <rosieliu@usc.edu>
2019-12-14 21:33:44 +0000

    i triedddd

--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,15 @@ const controller = require('../controllers/users.controller')
 router.get('/', controller.getAllUsers)
 
 module.exports = router;
+
+exports.getUsers = async function() {
+    var MongoClient = require('mongodb').MongoClient
+    let client = await MongoClient.connect(
+        'mongodb://0.0.0.0:27017',
+        {useUnifiedTopology: true}
+    )
+    let db = client.db('users')
+    let result = await db.collection("users").find().toArray()
+    client.close();
+    return result
+}
